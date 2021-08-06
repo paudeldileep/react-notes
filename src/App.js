07218e703c5e0c4adffcf5f880e888d20c90ce0c
paudeldileep <dileepkpaudel@gmail.com>
2021-08-06 22:58:10 +0545

    header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import NotesList from './components/Notes/NotesList';
 function App() {
   return (
     <div className="App w-full min-h-screen bg-gray-300">
-      <div className="h-14"></div>
+      <div className="h-14 bg-gradient-to-br from-green-400 to-green-700 font-bold text-gray-50 pt-2 pl-2 text-left"><h2 className="text-2xl font-mono">React-Notes</h2></div>
       <div className="box bg-gray-100 mt-10 pb-6">
        <NoteBox/>
        </div>
